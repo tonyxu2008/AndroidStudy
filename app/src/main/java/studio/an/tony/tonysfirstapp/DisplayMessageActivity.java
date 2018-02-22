995101bbe2c177e95c29ac6d5f6184d08de88b02
tonyxu2008 <35170638+tonyxu2008@users.noreply.github.com>
2018-02-23 07:46:04 +0800

    First
    First Version

---
+++ app/src/main/java/studio/an/tony/tonysfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,28 @@
+package studio.an.tony.tonysfirstapp;
+
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.content);
+        layout.addView(textView);
+    }
+}
